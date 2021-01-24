f032d71f65f099b3f3dd587a520e4f38334ce1ff
wexort <bugwriter@outlook.com>
2021-01-24 19:48:36 +0100

    add uuid as key for book in Shelf

--- src/components/BookGrid.js
+++ src/components/BookGrid.js
@@ -1,22 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { v4 as uuidv4 } from "uuid";
 
 import Book from "./Book";
 
 function BookGrid(props) {
   const { books } = props;
   return (
-    books !== undefined && (
-      <div>
-        <ol className="books-grid">
-          {books.map(book => (
-            <li>
-              <Book book />
-            </li>
-          ))}
-        </ol>
-      </div>
-    )
+    <div>
+      <ol className="books-grid" key={uuidv4()}>
+        {books.map(book => (
+          <li key={book.id}>
+            <Book book={book} />
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 }
+
+BookGrid.propTypes = {
+  books: PropTypes.array.isRequired
+};
 
 export default BookGrid;
