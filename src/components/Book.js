cae5fd0d36abac62e140e2a76263e5fae35b609b
wexort <bugwriter@outlook.com>
2021-01-24 19:47:23 +0100

    implement imageLink()

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,6 +4,18 @@ import PropTypes from "prop-types";
 class Book extends React.Component {
   render() {
     const { book } = this.props;
+    const imageLink = () => {
+      if (!book.imageLinks) {
+        return "";
+      }
+      if (book.imageLinks.thumbnail) {
+        return `url(${book.imageLinks.thumbnail})`;
+      }
+      if (book.imageLinks.smallThumbnail) {
+        return `url(${book.imageLinks.smallThumbnail})`;
+      }
+      return "";
+    };
     return (
       <div className="book">
         <div className="book-top">
@@ -12,8 +24,7 @@ class Book extends React.Component {
             style={{
               width: 128,
               height: 193,
-              backgroundImage:
-                'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
+              backgroundImage: imageLink()
             }}
           ></div>
           <div className="book-shelf-changer">
@@ -28,8 +39,10 @@ class Book extends React.Component {
             </select>
           </div>
         </div>
-        <div className="book-title">${book.title}</div>
-        <div className="book-authors">${book.author}</div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">
+          {Array.isArray(book.author) ? book.authors.join() : "Unknown"}
+        </div>
       </div>
     );
   }
