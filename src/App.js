a5efebb02d2eac1e1f74e825ba99a7e73aa90b97
wexort <bugwriter@outlook.com>
2021-01-25 08:43:58 +0100

    impl getAll books and display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, Route } from "react-router-dom";
 import "./App.css";
 import Shelf from "./components/Shelf";
 import Search from "./components/Search";
+import * as BooksAPI from "./BooksAPI";
 
 function BooksApp() {
+  const [allBooks, setAllBooks] = useState([]);
+  useEffect(() => {
+    BooksAPI.getAll().then(
+      books => setAllBooks(books),
+      err => console.log("error in BooksAPI.getAll", err)
+    );
+  });
+
   return (
     <div className="app">
       <div className="list-books-title">
@@ -21,9 +30,20 @@ function BooksApp() {
               </Link>
             </div>
             <div className="list-books-content">
-              <Shelf title="Currently Reading"></Shelf>
-              <Shelf title="Want to Read"></Shelf>
-              <Shelf title="Read"></Shelf>
+              <Shelf
+                name="Currently Reading"
+                books={allBooks.filter(
+                  book => book.shelf === "currentlyReading"
+                )}
+              ></Shelf>
+              <Shelf
+                name="Want to Read"
+                books={allBooks.filter(book => book.shelf === "wantToRead")}
+              ></Shelf>
+              <Shelf
+                name="Read"
+                books={allBooks.filter(book => book.shelf === "read")}
+              ></Shelf>
             </div>
           </div>
         )}
