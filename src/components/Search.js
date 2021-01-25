961e711fd7cfaece360a8dcb6d3b79442a52b7ed
wexort <bugwriter@outlook.com>
2021-01-25 21:35:56 +0100

    use functional components, handle empty Search correctly

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,76 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BookGrid from "./BookGrid";
 import * as BooksAPI from "../BooksAPI";
 import { Link } from "react-router-dom";
 
-class Search extends React.Component {
-  state = {
-    searchInput: "",
-    searchResult: [],
-    FetchError: ""
-  };
+function Search() {
+  const [searchInput, setSearchInput] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [FetchError, setFetchError] = useState("");
 
-  search = () => {
-    BooksAPI.search(this.state.searchInput).then(
-      books =>
-        books !== undefined &&
-        books.length !== 0 &&
-        Array.isArray(books) &&
-        this.setState(() => ({ FetchError: "", searchResult: books })),
-      err => this.setState(() => ({ FetchError: `error:${err}` }))
-    );
-  };
-
-  handleChange = event => {
+  const handleChange = event => {
     event.persist();
-    this.setState(
-      () => ({
-        searchInput: event.target.value
-      }),
-      this.search
-    );
+    setSearchInput(event.target.value);
   };
 
-  render() {
-    return (
-      <div>
-        <div className="search-books">
-          <div className="search-books-bar">
-            <Link to="/">
-              <button className="close-search">Close</button>
-            </Link>
-            <div className="search-books-input-wrapper">
-              {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-              <input
-                type="text"
-                placeholder="Search by title or author"
-                value={this.state.searchInput}
-                onChange={this.handleChange}
-              />
-            </div>
-          </div>
-          {this.state.FetchError !== "" && (
-            <div>
-              <span>Something going wrong: {this.state.FetchError}</span>
-            </div>
-          )}
-          <div className="search-books-results">
-            <ol className="books-grid"></ol>
+  useEffect(() => {
+    searchInput &&
+      BooksAPI.search(searchInput).then(
+        books =>
+          books !== undefined &&
+          books.length !== 0 &&
+          Array.isArray(books) &&
+          setSearchResult(books) &&
+          setFetchError(""),
+        err => setFetchError(err)
+      );
+  }, [searchInput]);
+  return (
+    <div>
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/">
+            <button className="close-search">Close</button>
+          </Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={searchInput}
+              onChange={handleChange}
+            />
           </div>
         </div>
-        {this.state.searchResult.length !== 0 && (
-          <BookGrid books={this.state.searchResult} />
+        {FetchError !== "" && (
+          <div>
+            <span>Something going wrong: {FetchError}</span>
+          </div>
         )}
+        <div className="search-books-results">
+          <ol className="books-grid"></ol>
+        </div>
       </div>
-    );
-  }
+      {searchInput && searchResult && <BookGrid books={searchResult} />}
+    </div>
+  );
 }
 
 export default Search;
